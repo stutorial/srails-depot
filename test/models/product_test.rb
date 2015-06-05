68f129b4a2afae3b2803ae9f51979179830e073a
Santiago <saantii.28@gmail.com>
2015-06-05 17:50:36 +0000

    Agrego validacion de presencia al atributo precio de product con test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
 
+  test "product with valid attributtes can be created" do
+    product = Product.new(title: "My title", description: "My description", image_url: "image_url.jpg", price: 10)
+    assert product.valid?
+  end
+  
   test "product attributtes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -10,5 +15,22 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:image_url].any?
     assert product.errors[:price].any?
   end  
+  
+  test "product price must be positive" do
+    product = Product.new(title: "My title", description: "My description", image_url: "image_url.jpg", price: -1)
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    
+    product.price = 0.001
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    
+    product.price = 1
+    assert product.valid?
+  end
   
 end
