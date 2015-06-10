dd9e62d61eb029279cd7038dcaa2b11783ac1d08
Santiago <saantii.28@gmail.com>
2015-06-10 16:58:05 +0000

    Agrego test para el modelo de Cart Valido que un cart pueda agregar un procuto con el metodo add_product

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -5,7 +5,7 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
-    assert_select '#main .entry', 1
+    assert_select '#main .entry', 2
     assert_select 'h3', 'Programming Ruby 1.9'
     assert_select '.price', /\$[,\d]+\.\d\d/
   end
