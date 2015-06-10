75009f35ee63b240932d566bab17f84b632b6766
Santiago <saantii.28@gmail.com>
2015-06-10 17:18:43 +0000

    Agrego manejo de error en todos los controllers Ahora se redirecciona al store si se quiere acceder a un producto/carrito/line_item que no existe en la base de datos

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,6 +2,7 @@ class LineItemsController < ApplicationController
   include CurrentCart
   before_action :set_cart, only: [:create]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_line_item
 
   # GET /line_items
   # GET /line_items.json
@@ -73,5 +74,10 @@ class LineItemsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
       params.require(:line_item).permit(:product_id, :cart_id)
+    end
+    
+    def invalid_line_item
+      logger.error "Attempt to access invalid line item #{params[:id]}"
+      redirect_to store_url, notice: "Invalid line item"
     end
 end
