4717351d8cc85aefeaabd15c5aadcfef88b99c7d
Santiago <saantii.28@gmail.com>
2015-06-10 14:00:18 +0000

    Agrego test para el modelo de Cart Valido que un cart con line items pueda ser eliminado y con el todos los line items

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "a cart can be deleted although it has line items" do
+    product = products(:ruby)
+    cart = carts(:cart_with_no_line_items)
+    LineItem.create(product: product, cart: cart)
+    
+    line_item_count = LineItem.count
+    cart_count = Cart.count
+    
+    cart.destroy
+    
+    assert_not_equal cart_count, Cart.count
+    assert_not_equal line_item_count, LineItem.count
+  end
 end
