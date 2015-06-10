92019e1f37dbc504c89e0a416d968e10b1ae0bd2
Santiago <saantii.28@gmail.com>
2015-06-10 16:50:19 +0000

    Modifico el Cart para que muestre los productos iguales juntos

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,12 @@
 class Cart < ActiveRecord::Base
     has_many :line_items, dependent: :destroy
+    
+    def add_product(product)
+      line_item = LineItem.find_by!(cart: self, product: product)
+      line_item.quantity += 1
+      line_item
+    rescue ActiveRecord::RecordNotFound
+        line_item = line_items.build(product: product)
+        line_item
+    end
 end
