564050360f7db91b635d57c30a2b33a26a1cb1da
Santiago <saantii.28@gmail.com>
2015-06-10 17:55:36 +0000

    Modifico la vista de Cart#show

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,10 @@
 class Cart < ActiveRecord::Base
     has_many :line_items, dependent: :destroy
     
+    def total_price
+        line_items.to_a.sum { |line_item| line_item.total_price }
+    end
+    
     def add_product(product)
       line_item = LineItem.find_by!(cart: self, product: product)
       line_item.quantity += 1
