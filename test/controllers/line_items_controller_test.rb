2f525ecb0481fc3410d24134fca208980e87d645
Santiago <saantii.28@gmail.com>
2015-06-11 17:26:32 +0000

    Bugfix en application.html Ahora pregunta si hay un carrito para cargarlo

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -23,7 +23,7 @@ class LineItemsControllerTest < ActionController::TestCase
       post :create, product_id: products(:ruby).id
     end
 
-    assert_redirected_to cart_path(assigns(:line_item).cart)
+    assert_redirected_to store_url
   end
 
   test "should show line_item" do
@@ -48,6 +48,6 @@ class LineItemsControllerTest < ActionController::TestCase
       delete :destroy, id: line_item
     end
 
-    assert_redirected_to @cart
+    assert_redirected_to store_url
   end
 end
