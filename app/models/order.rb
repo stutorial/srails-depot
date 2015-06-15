4023b7e2ee8f5d716c0da4ed3de15714ea01d775
Santiago <saantii.28@gmail.com>
2015-06-15 18:02:50 +0000

    Agrego funcionalidad para crear ordenes

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,15 @@ class Order < ActiveRecord::Base
     
     validates :name, :address, :email, :pay_type, presence: true
     validates :pay_type, inclusion: PAYMENT_TYPES
+    
+    has_many :line_items, dependent: :destroy
+    
+    def add_line_items_from_cart(cart)
+       cart.line_items.each do
+          |line_item|
+          line_item.order = self
+          line_item.cart = nil
+          line_items << line_item
+       end
+    end
 end
